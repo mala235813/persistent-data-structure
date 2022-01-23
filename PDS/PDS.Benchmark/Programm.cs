d2540617c6de981a4922bd05da38b67b1555f005
YGAR84 <a.lyamin@g.nsu.ru>
2022-01-23 17:41:21 +0700

    Add test bench

---
+++ PDS/PDS.Benchmark/Programm.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PDS.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
---
+++ PDS/PDS.Benchmark/TestBench.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using BenchmarkDotNet.Attributes;
+
+namespace PDS.Benchmark
+{
+    [DryJob]
+    [JsonExporterAttribute.Brief]
+    [JsonExporterAttribute.Full]
+    [JsonExporterAttribute.BriefCompressed]
+    [JsonExporterAttribute.FullCompressed]
+    [JsonExporter("-custom", indentJson: true, excludeMeasurements: true)]
+    public class TestBench
+    {
+        [Params(10)]
+        public int A { get; set; }
+
+        [Params(10)]
+        public int B { get; set; }
+
+        [Benchmark]
+        public void Benchmark()
+        {
+            Thread.Sleep(A + B + 5);
+        }
+    }
+}
