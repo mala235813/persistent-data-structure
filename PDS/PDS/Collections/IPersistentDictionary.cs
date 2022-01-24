f2707b89d8b9012f80d8b2457c221579fec3dc4d
6gales <6gales@gmail.com>
2022-01-24 18:24:35 +0700

    Add documentation for collections interfaces

--- PDS/PDS/Collections/IPersistentDictionary.cs
+++ PDS/PDS/Collections/IPersistentDictionary.cs
@@ -4,30 +4,96 @@ using System.Collections.Immutable;
 
 namespace PDS.Collections
 {
+    /// <summary>
+    /// Persistent dictionary
+    /// </summary>
+    /// <typeparam name="TKey">Key type</typeparam>
+    /// <typeparam name="TValue">Value type</typeparam>
     public interface IPersistentDictionary<TKey, TValue>
         : IPersistentDataStructure<KeyValuePair<TKey, TValue>, IPersistentDictionary<TKey, TValue>>,
             IImmutableDictionary<TKey, TValue>
     {
+        /// <summary>
+        /// Clear persistent dictionary
+        /// </summary>
+        /// <returns>Empty dictionary</returns>
         new IPersistentDictionary<TKey, TValue> Clear();
 
+        /// <summary>
+        /// Associate given key with given value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> Add(TKey key, TValue value);
         
+        /// <summary>
+        /// Add or update given key with given value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         IPersistentDictionary<TKey, TValue> AddOrUpdate(TKey key, TValue value);
         
+        /// <summary>
+        /// Update key using value factory
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="valueFactory">Function that provides value depending on key and old value</param>
+        /// <returns>New instance of persistent dictionary</returns>
         IPersistentDictionary<TKey, TValue> Update(TKey key, Func<TKey, TValue, TValue> valueFactory);
         
+        /// <summary>
+        /// Try associate given key with given value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="newVersion">New instance of persistent dictionary, or same instance if false</param>
+        /// <returns>True, if new value was added successfully</returns>
         bool TryAdd(TKey key, TValue value, out IPersistentDictionary<TKey, TValue> newVersion);
 
+        /// <summary>
+        /// Add range of key value pairs to dictionary
+        /// </summary>
+        /// <param name="pairs"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> AddRange(IEnumerable<KeyValuePair<TKey, TValue>> pairs);
 
+        /// <summary>
+        /// Set value for given key, same as AddOrUpdate
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> SetItem(TKey key, TValue value);
 
+        /// <summary>
+        /// Add range of key value pairs to dictionary
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> SetItems(IEnumerable<KeyValuePair<TKey, TValue>> items);
 
+        /// <summary>
+        /// Remove range of keys from dictionary
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> RemoveRange(IEnumerable<TKey> keys);
 
+        /// <summary>
+        /// Remove given key from dictionary
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>New instance of persistent dictionary</returns>
         new IPersistentDictionary<TKey, TValue> Remove(TKey key);
         
+        /// <summary>
+        /// Try remove given key from dictionary
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="newVersion">New instance of persistent dictionary, or same instance if false</param>
+        /// <returns>True, if new value was removed successfully</returns>
         bool TryRemove(TKey key, out IPersistentDictionary<TKey, TValue> newVersion);
     }
 }
--- PDS/PDS/Collections/IPersistentList.cs
+++ PDS/PDS/Collections/IPersistentList.cs
@@ -4,34 +4,121 @@ using System.Collections.Immutable;
 
 namespace PDS.Collections
 {
+    /// <summary>
+    /// Persistent array list with constant time random access
+    /// </summary>
+    /// <typeparam name="T">Item type</typeparam>
     public interface IPersistentList<T> : IPersistentDataStructure<T, IPersistentList<T>>, IImmutableList<T>
     {
+        /// <summary>
+        /// Add item to the end of the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> Add(T item);
         
+        /// <summary>
+        /// Add range of items to the end of the list
+        /// </summary>
+        /// <param name="items"></param> 
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> AddRange(IEnumerable<T> items);
-        
+
+        /// <summary>
+        /// Add range of items to the end of the list
+        /// </summary>
+        /// <param name="items"></param> 
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> AddRange(IReadOnlyCollection<T> items);
-        
+
+        /// <summary>
+        /// Locate given item in the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns>Index of item, or -1 if item was not found</returns>
         new int IndexOf(T item, int index, int count, IEqualityComparer<T>? equalityComparer);
         
+        /// <summary>
+        /// Insert item in given position
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> Insert(int index, T item);
 
+        /// <summary>
+        /// Locate given item from the end of the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns>Last index of item, or -1 if item was not found</returns>
         new int LastIndexOf(T item, int index, int count, IEqualityComparer<T>? equalityComparer);
 
+        /// <summary>
+        /// Remove first occurence of given item
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> Remove(T value, IEqualityComparer<T>? equalityComparer);
 
+        /// <summary>
+        /// Remove all items that match given predicate
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> RemoveAll(Predicate<T> match);
 
+        /// <summary>
+        /// Remove item from given position
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> RemoveAt(int index);
 
+        /// <summary>
+        /// Remove range of items
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> RemoveRange(IEnumerable<T> items, IEqualityComparer<T>? equalityComparer);
 
+        
+        /// <summary>
+        /// Remove count items starting from given index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> RemoveRange(int index, int count);
 
+        /// <summary>
+        /// Replace oldValue with newValue in list
+        /// </summary>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> Replace(T oldValue, T newValue, IEqualityComparer<T>? equalityComparer);
 
+        /// <summary>
+        /// Set value of item in given position
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        /// <returns>New instance of persistent list</returns>
         new IPersistentList<T> SetItem(int index, T value);
 
+        /// <summary>
+        /// Clear persistent list
+        /// </summary>
+        /// <returns>Empty list</returns>
         new IPersistentList<T> Clear();
     }
 }
