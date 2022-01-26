a224911a392c5caf3a231c965416879c28bbe58e
6gales <6gales@gmail.com>
2022-01-26 07:11:23 +0700

    Add immutable linked list and add by add test

--- PDS/PDS.Benchmark/ImmutableTreeListBenchmark.cs
+++ PDS/PDS.Benchmark/ImmutableTreeListBenchmark.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Immutable;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
+using Medallion.Collections;
 using PDS.Collections;
 using PDS.Implementation.Collections;
 using TunnelVisionLabs.Collections.Trees.Immutable;
@@ -18,8 +19,7 @@ namespace PDS.Benchmark
         [JsonExporter("-custom", indentJson: true, excludeMeasurements: true)]
         public class RangeToList
         {
-            [Params(10)]
-            public int Count { get; set; }
+            [Params(1000000)] public int Count { get; set; }
 
             [Benchmark(Baseline = true, Description = "ImmutableList")]
             public ImmutableList<int> List()
@@ -39,13 +39,78 @@ namespace PDS.Benchmark
                 return ImmutableTreeList.CreateRange(Enumerable.Range(0, Count));
             }
             
-            [Benchmark(Description = "IPersistentLinkedList")]
+            [Benchmark(Description = "ImmutableLinkedList")]
+            public ImmutableLinkedList<int> ImmutableLinkedList()
+            {
+                return ImmutableLinkedList<int>.Empty.AppendRange(Enumerable.Range(0, Count));
+            }
+
+            [Benchmark(Description = "PersistentLinkedList")]
             public IPersistentLinkedList<int> LinkedList()
             {
                 return new PersistentLinkedList<int>().AddRange(Enumerable.Range(0, Count));
             }
+
+            [Benchmark(Description = "PersistentList")]
+            public IPersistentList<int> PersistentList()
+            {
+                return new PersistentList<int>().AddRange(Enumerable.Range(0, Count));
+            }
+        }
+
+
+        [ShortRunJob]
+        [JsonExporterAttribute.Brief]
+        [JsonExporterAttribute.Full]
+        [JsonExporterAttribute.BriefCompressed]
+        [JsonExporterAttribute.FullCompressed]
+        [RPlotExporter]
+        [JsonExporter("-custom", indentJson: true, excludeMeasurements: true)]
+        public class AggregateAdd
+        {
+            [Params(100000)] public int Count { get; set; }
+
+            [Benchmark(Baseline = true, Description = "ImmutableList")]
+            public ImmutableList<int> List()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(ImmutableList<int>.Empty, (current, item) => current.Add(item));
+            }
+
+            [Benchmark(Description = "ImmutableArray")]
+            public ImmutableArray<int> Array()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(ImmutableArray<int>.Empty, (current, item) => current.Add(item));
+            }
+
+            [Benchmark(Description = "ImmutableTreeList")]
+            public ImmutableTreeList<int> TreeList()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(ImmutableTreeList<int>.Empty, (current, item) => current.Add(item));
+            }
             
-            //TODO: Add PersistentListRealisation
+            [Benchmark(Description = "ImmutableLinkedList")]
+            public ImmutableLinkedList<int> ImmutableLinkedList()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(ImmutableLinkedList<int>.Empty, (current, item) => current.Append(item));
+            }
+            
+            [Benchmark(Description = "PersistentLinkedList")]
+            public IPersistentLinkedList<int> LinkedList()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(new PersistentLinkedList<int>(), (current, item) => current.PushBack(item));
+            }
+
+            [Benchmark(Description = "PersistentList")]
+            public IPersistentList<int> PersistentList()
+            {
+                return Enumerable.Range(0, Count)
+                    .Aggregate(new PersistentList<int>(), (current, item) => current.Add(item));
+            }
         }
     }
 }
